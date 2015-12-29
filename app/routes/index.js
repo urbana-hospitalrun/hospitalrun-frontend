4029a59a88ba5aaa1b189b405e3e8524482845f1
nathanmcsu <nathan.mc.su@gmail.com>
2015-12-29 16:38:10 -0600

    added mock data types

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,43 @@ import Ember from 'ember';
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   afterModel: function() {
     this.controllerFor('navigation').set('allowSearch', false);
+  },
+
+  model: function(){
+    return{
+      data :[
+        {
+          name: "Advil",
+          quantity: 10,
+          inventoryType: "medication",
+          distributionUnit: "pcs",
+        },
+        {
+          name: "Tylenol",
+          quantity: 0,m
+          inventoryType: "medication",
+          distributionUnit: "pcs",
+        },
+        {
+          name: "face masks",
+          quantity: 100,
+          inventoryType: "supply",
+          distributionUnit: "pcs",
+        },
+        {
+          name: "syringes",
+          quantity: 20,
+          inventoryType: "supply",
+          distributionUnit: "pcs",
+        },
+        {
+          name: "pikachu",
+          quantity: 1,
+          inventoryType: "pokemon",
+          distributionUnit: "pkmn",
+        },
+      ]
+    };
   }
+
 });
