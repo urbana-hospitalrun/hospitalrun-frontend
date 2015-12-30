9d1a44d38fdc5fb61d32b32ecd19e24f70f54a46
smerdy <joshua.pham@gmail.com>
2015-12-30 14:51:58 -0600

    check app/routes/index.js for how to add mock data to index.hbs

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -35,4 +35,6 @@ export default Ember.Controller.extend(UserSession, {
     return activeLinks;
   }.property('indexLinks')
 
+
+
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,33 @@
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import Ember from 'ember';
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+import UserSession from 'hospitalrun/mixins/user-session'
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, UserSession, {
   afterModel: function() {
     this.controllerFor('navigation').set('allowSearch', false);
+  },
+  model: function() {
+    var userSession = this._getUserSessionVars();
+    var userRole = userSession.role;
+    console.log('My userRole is:', userRole);
+
+    switch (userRole) {
+      case 'System Administrator': // aka doctor
+        return [
+          { 'user': 'John Doe', 'disease': 'cancer' },
+          { 'user': 'John Doe', 'disease': 'cancer' },
+          { 'user': 'John Doe', 'disease': 'cancer' },
+          { 'user': 'John Doe', 'disease': 'cancer' },
+          { 'user': 'John Doe', 'disease': 'cancer' },
+          { 'user': 'John Doe', 'disease': 'cancer' },
+          { 'user': 'John Doe', 'disease': 'cancer' },
+          { 'user': 'John Doe', 'disease': 'cancer' },
+          { 'user': 'John Doe', 'disease': 'cancer' },
+          { 'user': 'John Doe', 'disease': 'cancer' },
+          { 'user': 'John Doe', 'disease': 'cancer' }
+        ];
+      default:
+        return null;
+    }
   }
 });
